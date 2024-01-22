1205bd2d00b125818c432446a494ba5ad980ef0b
Redpanda10 <121352679+Redpanda10@users.noreply.github.com>
2024-01-22 08:58:25 +0545

    Update deletion_single_linked_list.cpp

--- deletion_single_linked_list.cpp
+++ deletion_single_linked_list.cpp
@@ -7,10 +7,41 @@ class Node{
 };
 
 //deleting the node as (node_to_be _deleted, node_before)
-int delete_node(Node* &node,Node* &previous)
+int delete_node_any_position(Node* &node,Node* &previous)
 {
     previous->link = node->link;
     delete node;
+}
+//deleting the fist node
+int delete_at_start(Node* &head){
+    Node* temp=head;
+    head=head->link;
+    delete temp;
+}
+//deleting the last node
+int delete_at_end(Node* &head){
+    if (head==NULL)
+    cout<<"Empty list"<<endl;
+    else if(head->link==NULL)
+    {
+        delete head;
+        head==NULL;
+    }
+
+    else{
+        while (head->link->link!=NULL)
+        {
+            head=head->link;
+        }
+        if (head->link->link==NULL)
+        {   
+            delete head->link->link;
+            head->link=NULL;
+        }
+        
+        
+    }
+       
 }
 int display(Node* &head){
     Node* temp = head;
@@ -34,6 +65,7 @@ int main(){
     Node* two=new Node();
     Node* three=new Node();
     Node* four=new Node();
+    Node* five=new Node();
     
     one->value=32;
     one->link=two;
@@ -45,16 +77,25 @@ int main(){
     three->link=four;
 
     four->value=333;
-    four->link=NULL;
+    four->link=five;
+
+    five->value=555;
+    five->link=NULL;
 
     head=one;
 
     display(head);
 
-    delete_node(three,two);
-
+    delete_node_any_position(three,two);
     display(head);
 
 
+    delete_at_start(head);
+    display(head);
 
+    delete_at_end(head);
+    display(head);
+
+    delete_at_end(head);
+    //display(head);
 }
